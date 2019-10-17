d3b8ed9c868ccc5a73e26f41e881b882c5a15994
Viktor Silnov <vsblinov@alfabank.ru>
2019-10-17 12:11:28 +0300

    chore: add AccountService constraints and javadoc

--- src/main/kotlin/ru/victorpomidor/moneytransfer/service/AccountService.kt
+++ src/main/kotlin/ru/victorpomidor/moneytransfer/service/AccountService.kt
@@ -4,13 +4,31 @@ import ru.victorpomidor.moneytransfer.model.Account
 import ru.victorpomidor.moneytransfer.model.ChangeBalanceResult
 import java.math.BigDecimal
 import java.util.UUID
+import javax.validation.constraints.Positive
 
+/**
+ * Account CRU
+ */
 interface AccountService {
     fun getAccount(accountId: UUID): Account?
 
     fun createAccount(account: Account): Account
 
-    fun addBalance(accountId: UUID, value: BigDecimal): ChangeBalanceResult
+    /**
+     * Add a balance to account
+     * @param accountId account id
+     * @param value, should be positive
+     * @return ChangeBalanceResult.SUCCESS if success
+     * @throws IllegalArgumentException if value is <= 0
+     */
+    fun addBalance(accountId: UUID, @Positive value: BigDecimal): ChangeBalanceResult
 
-    fun subtractBalance(accountId: UUID, value: BigDecimal): ChangeBalanceResult
+    /**
+     * Subtract a balance from account
+     * @param accountId account id
+     * @param value, should be positive
+     * @return ChangeBalanceResult.SUCCESS if success, NOT_ENOUGH_BALANCE, if balance is too low
+     * @throws IllegalArgumentException if value is <= 0
+     */
+    fun subtractBalance(accountId: UUID, @Positive value: BigDecimal): ChangeBalanceResult
 }
